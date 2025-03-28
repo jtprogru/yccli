7fce13f7293ac5b72283c49cc1ff01059e933825
Mikhail Savin <jtprogru@gmail.com>
2025-03-28 19:14:47 +0300

    Initial commit
    Signed-off-by: Mikhail Savin <jtprogru@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+# yandex-cloud plugin zsh
+
+Simple plugin with some aliases for Yandex Cloud CLI.
+
+## License
+
+MIT
+
---
+++ yccli.plugin.zsh
@@ -0,0 +1,11 @@
+# Yandex Cloud CLI
+# Author: Mikhail Savin <jtprogru@gmail.com>
+# License: MIT
+
+alias yci='yc init '
+alias ycif='yc init --federation-id '
+alias ycv='yc version'
+alias ycu='yc components update'
+alias ycpl='yc config profile list'
+alias ycpa='yc config profile activate '
+alias yckms='yc kms '
